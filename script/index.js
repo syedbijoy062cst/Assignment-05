d90365b3ecbf80a13d9804c39eaa5c271c06360c
syedbijoy062cst <syedbijoy062@gmail.com>
2026-03-30 21:12:57 +0600

    home.html ar all html code complete.

--- script/index.js
+++ script/index.js
@@ -10,35 +10,37 @@ document.getElementById("login-btn")
 
     if(userName == "admin" && pin =="admin123"){
         alert('login Success');
-        window.location.assign("/Home.html")
+        window.location.href = "Home.html";
     }
     else{
         alert('login failed');
         return;
     }
     
+
+    document.getElementById("loadDataBtn").addEventListener("click", () => {
+  fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+
+      const container = document.getElementById("data-container");
+      container.innerHTML = "";
+
+      data.forEach(issue => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+          <h3>${issue.title || "No Title"}</h3>
+          <p>${issue.description || "No Description"}</p>
+          <hr/>
+        `;
+        container.appendChild(div);
+      });
+    })
+    .catch(err => {
+      console.error("Error:", err);
+    });
+});
     
 });
 
-let currentTab = "all";
-const tabActive = ["bg-primary"];
-const tabInactive = ["bg-transparent"];
-
-function switchTab(tab){
-    // console.log(tab);
-    const tabs = ["all", "Open", "Closed"];
-    currentTab = tab;
-    for(const t of tabs){
-        const tabName = document.getElementById("tab-" + t);
-        if(t === tab) {
-            tabName.classList.remove(...tabInactive);
-            tabName.classList.add(...tabActive);
-
-        }
-        else{
-            tabName.classList.remove(...tabActive);
-            tabName.classList.add(...tabInactive);
-        }
-    }
-};
-switchTab(currentTab);
